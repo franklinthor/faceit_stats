5db2f2f4457b1e963a319bd5d1ad10e83d3ef13e
franklinthor <31597952+franklinthor@users.noreply.github.com>
2021-03-04 19:24:45 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+    // src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    import React, {Component} from 'react';
 
-export default App;
+    class App extends Component {
+      render () {
+        return (
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            {/* <div id="faceitLogin"></div> */}
+          </div>
+        </div>
+        );
+      }
+    }
+
+    export default App;
